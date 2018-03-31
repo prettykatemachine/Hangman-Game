731fe1c84c7a8cd671d24a4daf8f8e8e5a87133c
prettykatemachine <katie.albee@gmail.com>
2018-03-30 21:57:28 -0500

    remaining guesses counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ var guess;
 var goodLetters = []; 
 var badLetters = []; 
 var counter = 0; //counts correct letters
-var remainingGuesses = 0;       // How many tries the player has left
+var remaining = 0;       // How many tries the player has left
 var hasFinished = false;        // 'press any key to try again'     
 var wins = 0;                   
 var winCount = document.getElementById("win-count");
@@ -37,19 +37,17 @@ function start() {
         }
       }
       
-      if (found) return; 
-      
+      if (found) return;  
    
       if (badLetters.indexOf(guess) < 0) {
         badLetters.push(guess);
         document.getElementById("wrongGuesses").innerHTML = badLetters.join(" ");
+        remaining--;
+        document.getElementById("remainingGuesses").innerHTML = remaining;
       }
 
-      if (remainingGuesses.indexOf(guess) < 0) {
-        badLetters.push(guess);
-        document.getElementById("remainingGuesses").losses++;
-      }
-//Somewhere in here, I need to figure out subtracting remainingGuesses from maxTries and once it loops through down to zero, the game is over.
+      
+//Once the remaining guesses are gone, or if the player guesses correctly, the game is over.
 //If the player loses, display youlose.jpg (sobbing mathematically meme)
 //If the player completes the world when the game is over, display kirk.jpg (the captain ready to party)
 //A reset button should happen, the game would reload to another random string in the array. Maybe an alert?
