9a88be022476c4ae0740a3f3060efdc62160e0a5
prettykatemachine <katie.albee@gmail.com>
2018-03-31 10:00:23 -0500

    Fixed a thing!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,12 @@
 //variables go here
 var answers = ["spock", "kirk", "enterprise", "picard"];
 
-const maxTries = 10;  //maximum failz
+var maxTries = 10;  //maximum failz
 var guess; 
 var goodLetters = []; 
 var badLetters = []; 
 var counter = 0; //counts correct letters
-var remaining = 0;       // How many tries the player has left
+var remaining = 10;       // How many tries the player has left
 var hasFinished = false;        // 'press any key to try again'     
 var wins = 0;                   
 var winCount = document.getElementById("win-count");
@@ -16,11 +16,14 @@ var word = answers[Math.floor(Math.random() * answers.length)];
 
 
 
+var letterCount = 0;
+
 function start() {
+    letterCount = word.length; // Initializing the letterCount to the length of the word
     for (i = 0; i < word.length; i++) {
       goodLetters[i] = "__";
     }
-  
+    document.getElementById("remainingGuesses").innerHTML = maxTries; // this will initialize the counter
     document.getElementById("answer").innerHTML = goodLetters.join(" ");
     console.log(word);
   }
@@ -29,25 +32,46 @@ function start() {
     document.onkeyup = function(event) {
       guess = event.key.toLowerCase();
       var found = false; 
+      remaining--;
       for (i = 0; i < word.length; i++) {
         if (guess === word[i]) {
           goodLetters[i] = guess;
           document.getElementById("answer").innerHTML = goodLetters.join(" ");
-          found = true;
+         
+         letterCount-- ; // subtracting from the count for successful tries
+          found = true;    
+              
         }
-      }
+     
       
-      if (found) return;  
+      }
+
+     
+
+      if(letterCount == 0){
+        alert("Win!");
+        //display Kirk
+      }
+
+      document.getElementById("remainingGuesses").innerHTML = remaining;
+
+      if (found) return; 
+      
    
       if (badLetters.indexOf(guess) < 0) {
         badLetters.push(guess);
         document.getElementById("wrongGuesses").innerHTML = badLetters.join(" ");
-        remaining--;
+      
         document.getElementById("remainingGuesses").innerHTML = remaining;
       }
 
-      
-//Once the remaining guesses are gone, or if the player guesses correctly, the game is over.
+      if (remaining <= 0){
+        alert("Damn");
+      }
+
+   
+
+//Somewhere in here, I need to figure out subtracting remainingGuesses from maxTries and once it loops through down to zero, the game is over.
 //If the player loses, display youlose.jpg (sobbing mathematically meme)
 //If the player completes the world when the game is over, display kirk.jpg (the captain ready to party)
 //A reset button should happen, the game would reload to another random string in the array. Maybe an alert?
